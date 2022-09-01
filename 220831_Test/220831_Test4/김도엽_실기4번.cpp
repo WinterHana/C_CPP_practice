8578c128bd586f1b872157a2b8b04f6a216e12b0
WinterHana <haha920@naver.com>
2022-09-01 11:57:42 +0900

    220901 Test modify

--- 220831_Test/220831_Test4/김도엽_실기4번.cpp
+++ 220831_Test/220831_Test4/김도엽_실기4번.cpp
@@ -2,6 +2,11 @@
 #include <string>
 #include <vector>
 using namespace std;
+/*
+* 현 문제 : guildList로 member가 넘어갈 때, 값의 복사가 일어남.
+* >> 포인터로 해결해야함
+* 
+*/
 // 1. 맴버
 class Member
 {
@@ -13,16 +18,24 @@ private:
 public:
 	string getID();
 	string getName();
+	string getGuildName();
 	void setName(string _name);	
 	void setGuildName(string _guildName);
 
 	Member(string _ID, string _nickname);
+	Member();
 };
 
 Member::Member(string _ID, string _nickname)
 {
 	ID = _ID;
 	nickname = _nickname;
+	guildName = "무소속";
+}
+Member::Member()
+{
+	ID = "none";
+	nickname = "none";
 	guildName = "무소속";
 }
 string Member::getID()
@@ -45,6 +58,10 @@ void Member::setGuildName(string _guildName)
 	this->guildName = _guildName;
 }
 
+string Member::getGuildName()
+{
+	return this->guildName;
+}
 // 2. 길드
 class Guild
 {
@@ -76,17 +93,19 @@ int main()
 	while (true)
 	{
 		cout << "명령 번호 / 아이디 / 닉네임 또는 길드명 /" << endl;
-		cin >> order >> ID >> name;
+		cin >> order;
 		switch (order)
 		{
 		case 1:
 		{
+			cin >> ID >> name;
 			Member member(ID, name);
 			memberList.push_back(member);
 			break;
 		}
 		case 2:
 		{
+			cin >> ID >> name;
 			for (int i = 0; i < memberList.size(); i++)
 			{
 				if (memberList[i].getID() == ID)
@@ -94,18 +113,93 @@ int main()
 					memberList[i].setName(name);
 				}
 			}
+			break;
 		}
 		case 3:
 		{
+			cin >> ID >> name;
+			bool MemberFind = false;
+			bool GuildFind = false;
+			Member* member;
+			for (int k = 0; k < memberList.size(); k++)
+			{
+				if (memberList[k].getID() == ID)
+				{
+					member = memberList[k];
+					MemberFind = true;
+					break;
+				}
+			}
+			
+			if (member.getName() == "none")
+			{
+				cout << "맴버를 찾지 못했습니다.";
+				break;
+			}
+
 			for (int i = 0; i < guildList.size(); i++)
 			{
 				if (guildList[i].getName() == name)
 				{
-
-					guildList[i].Members.push_back()
+					guildList[i].Members.push_back(member);
+					GuildFind = true;
+					break;
 				}
 			}
+			if (GuildFind == false)
+			{
+				Guild guild(name);
+				guild.Members.push_back(member);
+				guildList.push_back(guild);
+
+			}
+			break;
 		}
+		case 4:
+		{
+			if (guildList.size() <= 0) break;
+			for (int i = 0; i < guildList.size(); i++)
+			{
+				cout << guildList[i].getName() << endl;
+			}
+			break;
+		}
+		case 5:
+		{
+			cin >> name;
+			for (int i = 0; i < guildList.size(); i++)
+			{
+				if (guildList[i].getName() == name)
+				{
+					for (int j = 0; j < guildList[i].Members.size(); j++)
+					{
+						cout << guildList[i].Members[j].getName() << endl;
+					}
+				}
+				break;
+			}
+			break;
+		}
+		case 6:
+		{
+			cin >> ID;
+			for (int i = 0; i < memberList.size(); i++)
+			{
+				if (memberList[i].getID() == ID)
+				{
+					cout << memberList[i].getGuildName() << endl;
+					break;
+				}
+			}
+			break;
+		}
+		case -1:
+		{
+			return 0;
+		}
+
+		default:
+			break;
 		}
 	}
 
